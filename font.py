7b99d3411c85c5b0cf643240693f8e56fad94981
ru <rushil.cn@gmail.com>
2024-04-16 23:12:46 +0200

    Compiled project to be an actual package and fixed path system

--- font.py
+++ font.py
@@ -1,4 +1,3 @@
-
 import os
 
 import general_funcs as funcs
@@ -6,7 +5,6 @@ import pygame
 
 
 class Font:
-
     def __init__(self, file, bar_colour=(128, 128, 128), colourkey=(0, 0, 0)):
         self.fnt_img = pygame.image.load(file).convert()
         self.fnt_img.set_colorkey(colourkey)
@@ -19,54 +17,293 @@ class Font:
 
         for i in range(len(self.dist)):
             if i:
-                img = funcs.clip(self.fnt_img, self.dist[i - 1], 0, self.dist[i] - self.dist[i - 1], self.fnt_img.get_height())
+                img = funcs.clip(
+                    self.fnt_img,
+                    self.dist[i - 1],
+                    0,
+                    self.dist[i] - self.dist[i - 1],
+                    self.fnt_img.get_height(),
+                )
                 img.set_colorkey(colourkey)
                 self.imgs.append(img)
 
-        self.chars = {char: img for char, img in zip(['!', ',', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], self.imgs)}
-        self.chars[' '] = pygame.Surface((5, self.fnt_img.get_height()))
+        self.chars = {
+            char: img
+            for char, img in zip(
+                [
+                    "!",
+                    ",",
+                    "-",
+                    ".",
+                    "0",
+                    "1",
+                    "2",
+                    "3",
+                    "4",
+                    "5",
+                    "6",
+                    "7",
+                    "8",
+                    "9",
+                    ":",
+                    ";",
+                    "?",
+                    "A",
+                    "B",
+                    "C",
+                    "D",
+                    "E",
+                    "F",
+                    "G",
+                    "H",
+                    "I",
+                    "J",
+                    "K",
+                    "L",
+                    "M",
+                    "N",
+                    "O",
+                    "P",
+                    "Q",
+                    "R",
+                    "S",
+                    "T",
+                    "U",
+                    "V",
+                    "W",
+                    "X",
+                    "Y",
+                    "Z",
+                    "_",
+                    "a",
+                    "b",
+                    "c",
+                    "d",
+                    "e",
+                    "f",
+                    "g",
+                    "h",
+                    "x",
+                    "j",
+                    "k",
+                    "l",
+                    "m",
+                    "n",
+                    "y",
+                    "p",
+                    "q",
+                    "r",
+                    "s",
+                    "t",
+                    "u",
+                    "v",
+                    "w",
+                    "x",
+                    "y",
+                    "z",
+                ],
+                self.imgs,
+            )
+        }
+        self.chars[" "] = pygame.Surface((5, self.fnt_img.get_height()))
 
     def __getitem__(self, text):
         num_lines = 1
         if not isinstance(text, str):
-            raise TypeError('font can only take strings as text')
+            raise TypeError("font can only take strings as text")
         imgs = []
         for char in text:
-            if char not in ['\n', ' ', '!', ',', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
-                raise TypeError('text contains invalid character: ' + char)
-            if char == '\n':
+            if char not in [
+                "\n",
+                " ",
+                "!",
+                ",",
+                "-",
+                ".",
+                "0",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                ":",
+                ";",
+                "?",
+                "A",
+                "B",
+                "C",
+                "D",
+                "E",
+                "F",
+                "G",
+                "H",
+                "I",
+                "J",
+                "K",
+                "L",
+                "M",
+                "N",
+                "O",
+                "P",
+                "Q",
+                "R",
+                "S",
+                "T",
+                "U",
+                "V",
+                "W",
+                "X",
+                "Y",
+                "Z",
+                "_",
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+                "h",
+                "x",
+                "j",
+                "k",
+                "l",
+                "m",
+                "n",
+                "y",
+                "p",
+                "q",
+                "r",
+                "s",
+                "t",
+                "u",
+                "v",
+                "w",
+                "x",
+                "y",
+                "z",
+            ]:
+                raise TypeError("text contains invalid character: " + char)
+            if char == "\n":
                 num_lines += 1
             else:
                 imgs.append([self.chars[char], num_lines])
 
-        return_img = pygame.Surface((sum(img[0].get_width() for img in imgs), max(img[0].get_height() for img in imgs) * num_lines))
+        return_img = pygame.Surface(
+            (
+                sum(img[0].get_width() for img in imgs),
+                max(img[0].get_height() for img in imgs) * num_lines,
+            )
+        )
         x = 0
         for img in imgs:
-            return_img.blit(img[0], (x, -img[0].get_height() + img[0].get_height() * img[1]))
+            return_img.blit(
+                img[0], (x, -img[0].get_height() + img[0].get_height() * img[1])
+            )
             x += img[0].get_width()
         return return_img
 
     def render(self, text):
         num_lines = 1
         if not isinstance(text, str):
-            raise TypeError('font can only take strings as text')
+            raise TypeError("font can only take strings as text")
         imgs = []
         for char in text:
-            if char not in ['\n', ' ', '!', ',', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';',
-                            '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-                            'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
-                raise TypeError('text contains invalid character: ' + char)
-            if char == '\n':
+            if char not in [
+                "\n",
+                " ",
+                "!",
+                ",",
+                "-",
+                ".",
+                "0",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                ":",
+                ";",
+                "?",
+                "A",
+                "B",
+                "C",
+                "D",
+                "E",
+                "F",
+                "G",
+                "H",
+                "I",
+                "J",
+                "K",
+                "L",
+                "M",
+                "N",
+                "O",
+                "P",
+                "Q",
+                "R",
+                "S",
+                "T",
+                "U",
+                "V",
+                "W",
+                "X",
+                "Y",
+                "Z",
+                "_",
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+                "h",
+                "x",
+                "j",
+                "k",
+                "l",
+                "m",
+                "n",
+                "y",
+                "p",
+                "q",
+                "r",
+                "s",
+                "t",
+                "u",
+                "v",
+                "w",
+                "x",
+                "y",
+                "z",
+            ]:
+                raise TypeError("text contains invalid character: " + char)
+            if char == "\n":
                 num_lines += 1
             else:
                 imgs.append([self.chars[char], num_lines])
 
         return_img = pygame.Surface(
-            (sum(img[0].get_width() for img in imgs), max(img[0].get_height() for img in imgs) * num_lines))
+            (
+                sum(img[0].get_width() for img in imgs),
+                max(img[0].get_height() for img in imgs) * num_lines,
+            )
+        )
         x = 0
         for img in imgs:
-            return_img.blit(img[0], (x, -img[0].get_height() + img[0].get_height() * img[1]))
+            return_img.blit(
+                img[0], (x, -img[0].get_height() + img[0].get_height() * img[1])
+            )
             x += img[0].get_width()
         return return_img
 
@@ -75,10 +312,13 @@ SysFont = pygame.font.SysFont
 
 
 class FontManager:
-
     def __init__(self, path, bar_colour=(128, 128, 128), colourkey=(0, 0, 0)):
         self.path = path
-        self.fonts = {file.split('.')[0]: Font(path + os.sep + file, bar_colour, colourkey) for file in os.listdir(path) if file[0] != '.'}
+        self.fonts = {
+            file.split(".")[0]: Font(path + os.sep + file, bar_colour, colourkey)
+            for file in os.listdir(path)
+            if file[0] != "."
+        }
 
     def __getitem__(self, item):
         return self.fonts[item]
